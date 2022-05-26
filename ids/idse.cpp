76e0ae8493426be273c33a4b995cccdbf8bd6066
Chiranjeevi <cchimmili3@gatech.edu>
2022-05-26 18:49:10 -0400

    Comments added

--- ids/idse.cpp
+++ ids/idse.cpp
@@ -8,13 +8,15 @@
 
 using namespace std;
 
+jsonParser *parser = new jsonParser();
 vector<string> oligos;
-int numOligos;
 
 vector<string> generateOligosVector(ifstream &inFile) {
-    inFile.open(parser->input);         // Opens input FAFSTA file
+    // Opens input FAFSTA file
+    inFile.open(parser->input);         
     string line;
-    while (getline(inFile, line)) {     // Scans each line and creates vector of oligos
+    // Scans each line and creates vector of oligos
+    while (getline(inFile, line)) {     
         if (!line.empty()) {
             if (line[0] != '>') {
                 oligos.push_back(line);
@@ -22,39 +24,55 @@ vector<string> generateOligosVector(ifstream &inFile) {
         }
     }
     inFile.close();
-    numOligos = oligos.size();
     return oligos;
 }
 
 int generateIdsType() {
     double rnd = (double) randomIds(generator) / 100.0;
-    if (rnd <= errorWeights[0]) {     // Insert error
+
+    // Map of indelsub error percentenages
+    map<int, float> errorWeights = {
+    {0, parser->insertErrorRate}, {1, parser->deleteErrorRate}, {2, parser->substitutionErrorRate}, 
+    {3, 1 - parser->insertErrorRate - parser->deleteErrorRate - parser->substitutionErrorRate}
+    };
+
+    // Insert error
+    if (rnd <= errorWeights[0]) {     
         return 0;
     }
-    else if (rnd > errorWeights[0] && rnd <= errorWeights[0] + errorWeights[1] ) {    // Delete error
+    // Delete error
+    else if (rnd > errorWeights[0] && rnd <= errorWeights[0] + errorWeights[1] ) {   
         return 1;
     }
+    // Substitution Error
     else if (rnd > errorWeights[0] + errorWeights[1] && rnd <= errorWeights[0] + errorWeights[1] + errorWeights[2]) {
-        return 2;  // Substitution Error
+        return 2;  
     }
-    return 3;     // No Error
+    // No Error
+    return 3;    
 }
 
 string performIds(string str) {
+    // Error applied to each base in oligo 
     for (int i = 0; i < str.size(); i++) {
         int randIds = generateIdsType();
         char randBas = RANDOMBASE;
+        // Insert error applied
         if (randIds == 0) {
             str.insert(i, 1 , randBas);
             i++;
+        // Delete error applied
         } else if (randIds == 1) {
             str.erase(i, 1);
             i--;
+        // Substitution error applied
         } else if (randIds == 2) {
+            // Make sure substitution is not same base as old one
             while (randBas == str[i]) {
                 randBas = RANDOMBASE;
             }
             str[i] = randBas;
+        // No error applied
         } else {
             continue;
         }
@@ -63,18 +81,26 @@ string performIds(string str) {
 }
 
 void generateOutputFiles(ofstream &unorderedOutFile, ofstream &orderedOutFile, vector<string> oligos) {
+    // Open unordered and ordered files
     unorderedOutFile.open(parser->unorderedOutput, ios::out | ios::trunc);
     orderedOutFile.open(parser->orderedOutput, ios::out | ios::trunc);
     vector<pair<int, string>> ordered;
-    uniform_int_distribution<int> randomOligo(0, numOligos - 1);
+    uniform_int_distribution<int> randomOligo(0, parser->numberOligos - 1);
 
+    // Loop number of reads # of times
     for (int i = 0; i < parser->numberReads; i++) {
+        // Randomly select oligo from oligos vector (akin to a sequencer selecing a random oligo in a DNA pool)
         int oligoNumber = randomOligo(generator);
+        // Get strin with indelsub errors
         string errorStr = performIds(oligos[oligoNumber]);
+        // Maintain vector of oligos so we can create ordered output file
         ordered.emplace_back(oligoNumber + 1, errorStr);
+        // Add random oligo to unordered output file
         unorderedOutFile << ">" << (oligoNumber + 1) << "\n" << errorStr << endl;
     }
+    // Sort unordered oligo vector
     sort(ordered.begin(), ordered.end());
+    // Add each sorted oligo to ordered output file
     for (int i = 0; i < ordered.size(); i++) {
         orderedOutFile << ">" << ordered[i].first << "\n" << ordered[i].second << endl;
     }
--- include/idse.h
+++ include/idse.h
@@ -5,21 +5,20 @@
 using namespace std;
 
 
-jsonParser *parser = new jsonParser();
+
 random_device rand_dev;
 mt19937 generator(rand_dev());
 uniform_int_distribution<int> randomIds(1, 100);
+
 /* Macro that returns a random base from bases array */
 #define RANDOMBASE (bases[rand() % 4]);
+
 /*
 Insertion = 0, with default error occurence of 0%
 Deletion = 1
 Substitution = 2
 No error = 3
 */
-map<int, float> errorWeights = {
-    {0, 0.01}, {1, 0.02}, {2, 0.02}, {3, 0.95}
-};
 
 char bases[4] = {'A', 'T', 'C', 'G'};
 
